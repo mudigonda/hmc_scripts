37b6be65d507c0cf50c6f8da7ad20cc0dd9418db
mudigonda <mayur.mudigonda@gmail.com>
2016-01-29 19:39:50 -0800

    Added a test script to test out the autocorrelation function

--- test_fast_hmc.py
+++ test_fast_hmc.py
@@ -8,4 +8,13 @@ seed = 1234
 s_rng = T.shared_randomstreams.RandomStreams(seed)
 dim = np.array([2,10])
 rw = RoughWell(nbatch=dim[1])
-hmc.wrapper_hmc(s_rng=s_rng,energy_fn=rw.E_val,dim=dim)
+simulate = hmc.wrapper_hmc(s_rng=s_rng,energy_fn=rw.E_val,dim=dim)
+n_samples = 300 
+a = np.zeros([1,10,n_samples])
+b = np.zeros([2,10,n_samples])
+for ii in np.arange(n_samples):
+    a[:,:,ii],b[:,:,ii] = simulate()
+    print "Acceptance Rate"
+    print a[:,:,ii]
+    print "Samples"
+    print b[:,:,ii]
