5bc0b68692152c56b911cb74feeff11902d289d0
mudigonda <mayur.mudigonda@gmail.com>
2016-01-30 15:09:27 -0800

    This is the latest working version where it does not bork with syntax errors

--- test_ac.py
+++ test_ac.py
@@ -7,7 +7,7 @@ seed = 1234
 s_rng = T.shared_randomstreams.RandomStreams(seed)
 dim = np.array([2,10])
 rw = RoughWell(nbatch=dim[1])
-X = np.random.randn(2,100,50000).astype('float32')
+X = np.random.randn(2,100,5000).astype('float32')
 theano_ac = hmc.autocorrelation()
 ac= theano_ac(X)
 print ac
