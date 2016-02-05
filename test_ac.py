5f7dafc4c6d417846941d72b6b6dd462ecf8ea5f
Andrew Berger <andberger@berkeley.edu>
2016-02-05 11:16:03 -0800

    can i push

--- test_ac.py
+++ test_ac.py
@@ -2,12 +2,17 @@ from mjhmc.fast import hmc
 from mjhmc.fast.distributions_T import RoughWell
 import theano.tensor as T
 import numpy as np
+import time
 
 seed = 1234
 s_rng = T.shared_randomstreams.RandomStreams(seed)
 dim = np.array([2,10])
 rw = RoughWell(nbatch=dim[1])
 X = np.random.randn(2,100,5000).astype('float32')
+t1 = time.time()
 theano_ac = hmc.autocorrelation()
+t2 = time.time()
+print "elapsed time {}".format(t2 - t1)
 ac= theano_ac(X)
 print ac
+print "ac shape: {}".format(ac.shape)
