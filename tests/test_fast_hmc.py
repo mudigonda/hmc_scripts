7ca85af02fe52f5af61a5e416d833ef8f9505419
mudigonda <mayur.mudigonda@gmail.com>
2016-04-19 18:37:29 -0700

    Some reorganization. Some old commits

--- tests/test_fast_hmc.py
+++ tests/test_fast_hmc.py
@@ -3,18 +3,24 @@ from mjhmc.fast import hmc
 from mjhmc.fast.distributions_T import RoughWell
 import theano.tensor as T
 import numpy as np
+import time
 
+tm1 = time.time()
 seed = 1234
 s_rng = T.shared_randomstreams.RandomStreams(seed)
-dim = np.array([2,10])
-rw = RoughWell(nbatch=dim[1])
-simulate = hmc.wrapper_hmc(s_rng=s_rng,energy_fn=rw.E_val,dim=dim)
-n_samples = 300 
-a = np.zeros([1,10,n_samples])
-b = np.zeros([2,10,n_samples])
+ndims  = 64
+nbatch = 250 
+rw = RoughWell(nbatch=nbatch)
+simulate = hmc.wrapper_hmc(s_rng=s_rng,energy_fn=rw.E_val,dim=[ndims,nbatch])
+n_samples = 1000 
+a = np.zeros([1,nbatch,n_samples])
+b = np.zeros([ndims,nbatch,n_samples])
 for ii in np.arange(n_samples):
     a[:,:,ii],b[:,:,ii] = simulate()
     print "Acceptance Rate"
     print a[:,:,ii]
     print "Samples"
     print b[:,:,ii]
+
+tm2 = time.time()
+print tm2-tm1
