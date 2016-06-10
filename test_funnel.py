6bd6c2005f158863d1e4921f9fb7550970eb23ef
mudigonda <mayur.mudigonda@gmail.com>
2016-06-10 15:40:28 -0700

    added a line to make sure scale is being set correctly

--- test_funnel.py
+++ test_funnel.py
@@ -8,11 +8,12 @@ import theano.tensor as T
 
 X = np.random.randn(10,5).astype('float32')
 
-f = Funnel(nbatch=15)
+f = Funnel(nbatch=15,scale=1.0)
 print X
 print f.E_val(X)
 print f.dEdX_val(X)
 print f.nbatch
+print f.scale
 mjhmc = MarkovJumpHMC(distribution=f)
 print("Testing if it can generate a few samples")
 for ii in range(5):
