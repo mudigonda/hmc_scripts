5e3ad22be1adf407b220ad8bc6f39ad5e8750aec
mudigonda <mayur.mudigonda@gmail.com>
2016-06-28 17:02:36 -0700

    Fixed the test funnel to also sample only from 2 dimensions

--- sample_from_poe.py
+++ sample_from_poe.py
@@ -5,8 +5,9 @@ import numpy as np
 
 
 BURN_IN_STEPS = int(1E+3)
-BATCH_SIZE = int(1E+2)
+BATCH_SIZE = int(1E+1)
 
 #Not doing this makes it bork crazily. Poor implementation of inheritance
 mjhmc = MarkovJumpHMC(distribution=ProductOfT(nbatch=BATCH_SIZE))
 mjhmc.sampling_iteration()
+import IPython; IPython.embed()
--- test_funnel.py
+++ test_funnel.py
@@ -1,20 +1,29 @@
 from mjhmc.misc.distributions import Funnel
 from mjhmc.samplers.markov_jump_hmc import MarkovJumpHMC
+from mjhmc.misc.plotting import hist_2d
 import numpy as np
 import theano
 import theano.tensor as T
 
 
+nbatch = 15
+scale = 1.0
+ndims = 2
+X = np.random.randn(ndims,nbatch).astype('float32')
 
-X = np.random.randn(10,5).astype('float32')
-
-f = Funnel(nbatch=15,scale=1.0)
+f = Funnel(nbatch=nbatch,scale=scale,ndims=ndims)
 print X
 print f.E_val(X)
+print f.E_val(X).shape
 print f.dEdX_val(X)
+print f.dEdX_val(X).shape
 print f.nbatch
 print f.scale
 mjhmc = MarkovJumpHMC(distribution=f)
+'''
 print("Testing if it can generate a few samples")
 for ii in range(5):
     print(mjhmc.sample())
+'''
+fig = hist_2d(f,10000)
+import IPython; IPython.embed()
