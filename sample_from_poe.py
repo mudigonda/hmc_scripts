31dbf5096553e4f65e2fad3c5f84ad6bbb8204f6
mudigonda <mayur.mudigonda@gmail.com>
2016-07-14 04:51:21 -0700

    used the same script for both random W and random sparse W but it's the same idea basically

--- sample_from_poe.py
+++ sample_from_poe.py
@@ -2,12 +2,24 @@ from mjhmc.samplers.markov_jump_hmc import MarkovJumpHMC
 from mjhmc.misc.autocor import calculate_autocorrelation
 from mjhmc.misc.distributions import ProductOfT
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from scipy.sparse import rand
 
 
+np.random.seed(2015)
 BURN_IN_STEPS = int(1E+3)
 BATCH_SIZE = int(1E+1)
+nbasis = 36
+ndims = 36
+W_half = rand(ndims,nbasis/2,density=0.35)
+W = np.concatenate((W_half.todense(),-W_half.todense()),axis=1)
 
 #Not doing this makes it bork crazily. Poor implementation of inheritance
-mjhmc = MarkovJumpHMC(distribution=ProductOfT(nbatch=BATCH_SIZE))
-mjhmc.sampling_iteration()
+PoT = ProductOfT(nbasis=nbasis,ndims=ndims,W=W)
+ac_df = calculate_autocorrelation(MarkovJumpHMC,PoT,num_steps=1000)
 import IPython; IPython.embed()
+plt.plot(ac_df['autocorrelation'])
+plt.title('Random_Sparse_W_AC')
+plt.savefig('ac_rand_sparse_poe_w.png')
